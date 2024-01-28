1b962abe4131772ba677086d7285122b0eb4cd95
Nirav Soni <soninirav910@gmail.com>
2024-01-28 19:00:17 +0530

    Add project files.

---
+++ Business/Models/Order.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Strategy_Pattern_Using_different_shipping_providers.Business.Models
+{
+    public class Order
+    {
+        public Dictionary<Item, int> LineItems { get; } = new Dictionary<Item, int>();
+
+        public decimal TotalPrice => LineItems.Sum(item => item.Key.Price * item.Value);
+
+        public ShippingDetails ShippingDetails { get; set; }
+
+    }
+    public class ShippingDetails
+    {
+        public string Receiver { get; set; }
+
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string DestinationCountry { get; set; }
+        public string DestinationState { get; set; }
+
+        public string OriginCountry { get; set; }
+        public string OriginState { get; set; }
+    }
+
+    public class Item
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public ItemType ItemType { get; set; }
+
+        //public decimal GetTax()
+        //{
+        //    switch (ItemType)
+        //    {
+        //        case ItemType.Service:
+        //        case ItemType.Food:
+        //        case ItemType.Hardware:
+        //        case ItemType.Literature:
+        //        default:
+        //            return 0m;
+        //    }
+        //}
+
+        public Item(string id, string name, decimal price, ItemType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ItemType = type;
+        }
+    }
+
+    public enum ItemType
+    {
+        Service,
+        Food,
+        Hardware,
+        Literature
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using Strategy_Pattern_Using_different_shipping_providers.Business.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Strategy_Pattern_Using_different_shipping_providers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = new[] {
+                new Order
+                {
+                    ShippingDetails = new ShippingDetails
+                    {
+                        OriginCountry = "Sweden"
+                    }
+                },
+                new Order
+                {
+                    ShippingDetails = new ShippingDetails
+                    {
+                        OriginCountry = "USA"
+                    }
+                },
+                new Order
+                {
+                    ShippingDetails = new ShippingDetails
+                    {
+                        OriginCountry = "Sweden"
+                    }
+                },
+                new Order
+                {
+                    ShippingDetails = new ShippingDetails
+                    {
+                        OriginCountry = "USA"
+                    }
+                },
+                new Order
+                {
+                    ShippingDetails = new ShippingDetails
+                    {
+                        OriginCountry = "Singapore"
+                    }
+                }
+            };
+
+            Print(orders);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorting..");
+            Console.WriteLine();
+
+            /// TODO: Sort array with strategies implementing IComparer<T>
+
+            Array.Sort(orders, new OrderAmountComparer());
+            Console.WriteLine("Sorted based on order amount");
+            Console.WriteLine();
+            Print(orders);
+            Console.WriteLine();
+            Array.Sort(orders, new OrderOriginComparer());
+            Console.WriteLine("Sorted based on origin");
+            Console.WriteLine();
+            Print(orders);
+        }
+
+        static void Print(IEnumerable<Order> orders)
+        {
+            foreach (var order in orders)
+            {
+                Console.WriteLine(order.ShippingDetails.OriginCountry);
+            }
+        }
+    }
+
+    #region Strategies
+    /// <summary>
+    /// Sorts order based on Amount
+    /// </summary>
+    public class OrderAmountComparer : IComparer<Order>
+    {
+        public int Compare(Order x, Order y)
+        {
+            var xTotal = x.TotalPrice;
+            var yTotal = y.TotalPrice;
+            if (xTotal == yTotal)
+            {
+                return 0;
+            }
+            else if (xTotal > yTotal)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+
+    /// <summary>
+    /// Sorts orders based on Country of Origin
+    /// </summary>
+    public class OrderOriginComparer : IComparer<Order>
+    {
+        public int Compare(Order x, Order y)
+        {
+            var xDest = x.ShippingDetails.OriginCountry.ToLowerInvariant();
+            var yDest = y.ShippingDetails.OriginCountry.ToLowerInvariant();
+            if (xDest == yDest)
+            {
+                return 0;
+            }
+            else if(xDest[0] > yDest[0])
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+    #endregion
+}
